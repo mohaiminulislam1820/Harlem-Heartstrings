e52b7f3dd298da74d2693aaacabd3b13f7d02e8d
aaachris1820 <63766879+aaachris1820@users.noreply.github.com>
2023-06-07 20:06:29 +0600

    added conditional render in navbar for dashboard and profile photo

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,16 +1,20 @@
 import { useNavigate } from "react-router-dom";
 import ActiveLink from "./ActiveLink";
+import { useContext } from "react";
+import { Contexts } from "../Context/ContextWrapper";
 
 
 const Navbar = () => {
 
-    const navigate=useNavigate();
+    const navigate = useNavigate();
+
+    const { user, signOutUser } = useContext(Contexts);
 
     return (
         <nav className="container my-10 flex justify-between flex-wrap items-center gap-x-32 gap-y-6">
-            <h1 className="font-bold text-4xl text-[#F45050] cursor-pointer" onClick={()=>navigate('/')}>Harlem Heartstrings</h1>
+            <h1 className="font-bold text-4xl text-[#F45050] cursor-pointer" onClick={() => navigate('/')}>Harlem Heartstrings</h1>
 
-            <div className="flex flex-wrap gap-6">
+            <div className="flex flex-wrap gap-6 items-center">
 
                 <ActiveLink to={''} >Home</ActiveLink>
 
@@ -18,9 +22,20 @@ const Navbar = () => {
 
                 <ActiveLink to={''} >Classes</ActiveLink>
 
-                <ActiveLink to={''} >Dashboard</ActiveLink>
+                {
+                    user
+                        ? <>
+                            <ActiveLink to={''} >Dashboard</ActiveLink>
+                            <img src={user.photoURL || 'https://i.ibb.co/FzdHYpH/7518698-avatar-people-user-icon-1.png'} alt="user icon" className="rounded-full object-cover w-12" />
 
-                <button onClick={()=>navigate('/login')}>Login</button>
+                            <button onClick={signOutUser}>Sign Out</button>
+                        </>
+                        : <button onClick={() => navigate('/login')}>Login</button>
+                }
+
+
+
+
 
             </div>
 
