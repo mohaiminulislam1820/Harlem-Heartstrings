acbfa6636cd3690b5192fb20f24dc1194aea4b5c
aaachris1820 <63766879+aaachris1820@users.noreply.github.com>
2023-06-07 20:41:37 +0600

    implemented a toggle button on password input field to show and hide password

--- src/Components/Login-Register/Login.jsx
+++ src/Components/Login-Register/Login.jsx
@@ -9,6 +9,8 @@ const Login = () => {
 
     const [errorMsg, setErrorMsg] = useState('');
 
+    const [showPassword,setShowPassword]=useState(false);
+
     const { signInWithEmail, signInWithGoogle, setLoading } = useContext(Contexts);
 
     const location = useLocation();
@@ -44,6 +46,19 @@ const Login = () => {
 
     }
 
+    const handleGoogleSignIn = async () => {
+        try {
+            const loggedUser = await signInWithGoogle();
+
+            loggedUser.user && navigate(from, { replace: true });
+        }
+
+        catch (error) {
+            errorMsgHandler(error.code);
+        }
+
+    }
+
     return (
         <section className="container grid grid-cols-1 md:grid-cols-2 items-center gap-6 mt-20">
 
@@ -68,7 +83,11 @@ const Login = () => {
                     <div className="flex flex-col gap-2 mb-4">
                         <label htmlFor="password" className="font-semibold">Password</label>
 
-                        <input type="password" name="password" className="auth-input" placeholder="password" />
+                        <div className="relative flex flex-col">
+                            <input type={showPassword?'text':'password'} name="password" className="auth-input appearance-none" placeholder="password" />
+                            <img src="https://i.ibb.co/Ypc26t9/visibility-FILL0-wght400-GRAD0-opsz48.png" alt="visibility eye icon" className="w-6 absolute top-2 right-1 " onClick={()=>setShowPassword(!showPassword)} />
+                        </div>
+
                     </div>
 
                     <button className="bg-[#F9D949] mt-4 btn-regular" type="submit">Sign In</button>
@@ -81,7 +100,7 @@ const Login = () => {
                 <h1>Don't have an account? <span className="text-[#f45050] font-semibold cursor-pointer" onClick={() => navigate('/register')}>Sign Up</span> here</h1>
 
                 <div className="flex justify-center">
-                    <button className="mt-8 border-2 px-8 py-3  font-bold rounded-lg shadow-lg"> <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google icon" className="w-8 inline bg-white rounded-full mr-2" /> Sign In with Google</button>
+                    <button className="mt-8 border-2 px-8 py-3  font-bold rounded-lg shadow-lg" onClick={handleGoogleSignIn}> <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google icon" className="w-8 inline bg-white rounded-full mr-2" /> Sign In with Google</button>
                 </div>
             </div>
 
