cf5c738e2cad8a69c18a3971ec68c74c127bd034
aaachris1820 <63766879+aaachris1820@users.noreply.github.com>
2023-06-12 22:09:19 +0600

    slight css fix

--- src/Components/OtherPages/InstuctorCard.jsx
+++ src/Components/OtherPages/InstuctorCard.jsx
@@ -3,7 +3,7 @@ const InstuctorCard = ({ instructor }) => {
     const { name, email, image } = instructor;
     return (
         <div className="border rounded-lg shadow-sm p-4">
-            <img src={image} alt="profile photo" className="object-cover h-[300px] mb-8" />
+            <img src={image} alt="profile photo" className="object-cover h-[300px] mb-8 w-full" />
 
             <p><span className="font-semibold">Name : </span>{name}</p>
 
